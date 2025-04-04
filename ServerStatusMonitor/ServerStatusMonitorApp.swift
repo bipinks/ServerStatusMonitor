3f1129c916f36cfc11bf647c03bff3ab73041469
Bipin Kareparambil <mr.bipinks@gmail.com>
2025-04-04 17:15:31 +0400

    Initial Commit

---
+++ ServerStatusMonitor/ServerStatusMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServerStatusMonitorApp.swift
+//  ServerStatusMonitor
+//
+//  Created by Bipin Kareparambil on 04/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct ServerStatusMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ServerStatusMonitorTests/ServerStatusMonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ServerStatusMonitorTests.swift
+//  ServerStatusMonitorTests
+//
+//  Created by Bipin Kareparambil on 04/04/2025.
+//
+
+import Testing
+@testable import ServerStatusMonitor
+
+struct ServerStatusMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
